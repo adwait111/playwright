55c36cd59151bdcf407cb1de6d2c109df8e65c6c
adwait111 <suvarnarahane111@gmail,com>
2024-03-26 20:34:54 +0530

    added

--- tests/TC10Alerts.spec.js
+++ tests/TC10Alerts.spec.js
@@ -19,11 +19,16 @@ test('Handle js confirm alert with playwright', async ({ page }) => {
     page.on('dialog', async confirmAlert => {
         await expect(confirmAlert.message()).toContain('I am a JS Confirm')
         await expect(confirmAlert.type()).toContain('confirm')
-        await confirmAlert.accept()
+        // console.log(confirmAlert.message())
+        // console.log(confirmAlert.type())
+        //await confirmAlert.accept()
+        confirmAlert.dismiss()
     })
 
     await page.locator('button[onclick="jsConfirm()"]').click()
-    await expect(page.locator('#result')).toHaveText('You clicked: Ok')
+    //await expect(page.locator('#result')).toHaveText('You clicked: Ok')
+    await expect(page.locator('#result')).toHaveText('You clicked: Cancel')
+    await page.waitForTimeout(4000)
 })
 
 test.only('Handle js Prompt alert with playwright', async ({ page }) => {
@@ -31,9 +36,14 @@ test.only('Handle js Prompt alert with playwright', async ({ page }) => {
     page.on('dialog', async promptAlert => {
         await expect(promptAlert.message()).toContain('I am a JS prompt')
         await expect(promptAlert.type()).toContain('prompt')
-        await promptAlert.accept('suvarna')
+        // console.log(promptAlert.message())
+        // console.log(promptAlert.type())
+        //await promptAlert.accept('suvarna')
+        promptAlert.dismiss()
     })
 
     await page.locator('button[onclick="jsPrompt()"]').click()
-    await expect(page.locator('#result')).toHaveText('You entered: suvarna')
+    //await expect(page.locator('#result')).toHaveText('You entered: suvarna')
+    await expect(page.locator('#result')).toHaveText('You entered: null')
+    await page.waitForTimeout(4000)
 })
